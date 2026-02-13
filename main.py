cd46aca22e75e4cbdbb24c4d5e59e03cb90c9098
Talyshprodigy <orbitalfanmusic@gmail.com>
2026-02-13 18:05:00 +0000

    added button. doesnt do anything yet

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 ﻿import math
 import tkinter as tk
 
+
+
+
+
 window = tk.Tk()
 window.configure(background="white")
 
-tk.Label(window, text="Your function:").pack(padx=10, pady=(10, 0))
 
-name_entry = tk.Entry(window, width=300)  # input box
-name_entry.pack(padx=10, pady=10)
-name_entry.focus()
-
-canvas = tk.Canvas(window, width=2000, height=2000)
-canvas.pack()
 
 DEFAULT_STYLE = {"outline": "green", "fill": "yellow", "width": 3}
 
@@ -27,14 +24,52 @@ def lines():
         canvas.create_polygon([1020, i * 30, 980, i * 30], **DEFAULT_STYLE)
 
 
+
+
+
+
+
+
+
+
+tk.Label(window, text="Your function:").pack(padx=10, pady=(10, 0))
+name_entry = tk.Entry(window, width=300)  # input box
+name_entry.pack(padx=10, pady=10)
+name_entry.focus()
+
+def function():
+    points = []
+    if name_entry.get()[:2] != "y =":
+        print("wrong input")
+        return
+    else:
+        pass
+
+    for i in range(-500, 500):
+        x_px = i*3+1000
+
+
+
+function_confirm_button = tk.Button(window, text="Confirm Function", command=function)
+function_confirm_button.pack(pady=10)
+
+canvas = tk.Canvas(window, width=2000, height=2000)
+canvas.pack()
+
+
+
 def yequalsx():
     points = []
     for i in range(-500, 500):
+
         x_px = i * 3 + 1000
         y_px = math.cos(i / 20) * 200 + 700
         points.extend([x_px, y_px])  # flatten into [x1, y1, x2, y2, ...]
 
     canvas.create_line(*points, fill="green", width=3, smooth=True)
+
+
+
 
 
 
